a74b585a704648ceaf1273e49e49d0d639b64ff7
Kryptonite <kryptonite@Kryptonites-iMac.local>
2015-05-29 09:05:16 +0800

    Initial Commit

---
+++ FCO Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FCO Test
+//
+//  Created by Kryptonite on 5/29/15.
+//  Copyright (c) 2015 Kryptonite. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FCO Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FCO Test
+//
+//  Created by Kryptonite on 5/29/15.
+//  Copyright (c) 2015 Kryptonite. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
