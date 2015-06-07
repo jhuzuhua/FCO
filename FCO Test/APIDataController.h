084755b2c5f1295ed40b51841bf40a865845e932
Joshua N. Paldo <joshuapaldo@yahoo.com>
2015-06-08 02:39:54 +0800

    API Connection and Singleton DataController.

---
+++ FCO Test/APIDataController.h
@@ -0,0 +1,20 @@
+//
+//  APIDataController.h
+//  FCO Test
+//
+//  Created by Kryptonite on 6/8/15.
+//  Copyright (c) 2015 Kryptonite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AFNetworking.h>
+
+@interface APIDataController : NSObject{
+    
+}
+
++ (APIDataController *)getInstance;
+
+-(void)loginUserWithParameters;
+
+@end
